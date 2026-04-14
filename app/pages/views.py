417351fe188513103226267d9ddda53beba8d6a3
nenko9 <drif649@gmail.com>
2026-04-14 14:40:22 +0300

    feat: add styles

--- app/pages/views.py
+++ app/pages/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Index world")
 
 def wellcome(request):
-    return HttpResponse("Wellcome page")
+    return render(request, "pages/wellcome.html")
 
 def login(request):
     return render(request, "pages/login.html")
